4cc392beae6e0c16a0414ef896c4db611646084a
vynexsolutionsofficial <vynexsolutionsofficial@gmail.com>
2026-05-26 15:17:56 +0530

    Refactor: Copy Kresai's exact cyber space theme, animated glow mesh, and typography into Hackflow portfolio

--- src/components/BackgroundEffects.tsx
+++ src/components/BackgroundEffects.tsx
@@ -3,10 +3,10 @@ import './BackgroundEffects.css'
 export default function BackgroundEffects() {
   return (
     <div className="bg-effects-container">
-      {/* Dynamic Floating Glow Orbs */}
-      <div className="bg-orb orb-indigo" />
-      <div className="bg-orb orb-purple" />
+      {/* Kresai Brand Dynamic Floating Glow Orbs */}
       <div className="bg-orb orb-pink" />
+      <div className="bg-orb orb-purple" />
+      <div className="bg-orb orb-blue" />
       
       {/* High-End Digital Dot Matrix Overlay */}
       <div className="grid-overlay" />
